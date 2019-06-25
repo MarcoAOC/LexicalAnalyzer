2ad610b35791ae42aeb68286f6b24e9d2290a4d2
MarcoAOC <marcoaoc.ufop@outlook.com>
2019-06-25 16:19:16 -0300

    OutputCorrection

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ a = AnaLex("C:/Users/marco/Documents/GitHub/LexicalAnalyzer/main.txt")
 tokens = a.AnaLex()
 
 for i in tokens:
-    print("<"+str(i.line)+",'"+i.content+"','"+i.att+"')")
+    print("<"+str(i.line)+",'"+i.content+"','"+i.att+"'>")
 
 
